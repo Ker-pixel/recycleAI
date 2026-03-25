fc27f18be7dd00e7d63adcc19ce9ae7cd6150313
Kerem <kerem@Kerems-MacBook-Air.local>
2026-03-25 16:42:31 +0300

    Bug Fixes

--- src/model.py
+++ src/model.py
@@ -1,9 +1,10 @@
+# src/model.py
 import torch.nn as nn
 from torchvision import models
 
 def build_model():
-    # weights=None (modern standard) prevents downloading pretrained ImageNet weights
-    model = models.resnet18(weights=None)
+    # Using pretrained=False is safer across different torchvision versions
+    model = models.resnet18(pretrained=False) 
     num_ftrs = model.fc.in_features
     model.fc = nn.Linear(num_ftrs, 2)
     return model
